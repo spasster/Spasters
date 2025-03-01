d78ad5f831592556749b4efb95f91e52f887669b
spasster <nelidanila7@gmail.com>
2025-03-01 15:15:45 +0300

    123

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 import authentaction.views as views 
 
 urlpatterns = [
-    path('create_product/', views.create_product, name='create_product'),
+    # path('create_product/', views.create_product, name='create_product'),
 ]
